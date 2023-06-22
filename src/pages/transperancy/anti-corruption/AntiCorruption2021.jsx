972f0d24ad1c52884d26baa8c1ca6f053bc2313c
Mokka <110095485+Mokkaliona@users.noreply.github.com>
2023-06-22 15:03:30 +0800

    SuheeBolSugaSda

---
+++ src/pages/transperancy/anti-corruption/AntiCorruption2021.jsx
@@ -0,0 +1,6 @@
+export default function AntiCorruption2021(){
+    return(
+        <>
+        </>
+    )
+}
---
+++ src/pages/transperancy/financialsituation/Report2022.jsx
@@ -0,0 +1,6 @@
+export default function Report2022(){
+    return(
+        <>
+        </>
+    )
+}
