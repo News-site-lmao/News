fc02be5446520b2ab6dc9a14a1f3ee0105f0dfea
munkhtavilan <munkhtavilanbusiness@gmail.com>
2023-06-26 12:59:55 +0800

    news-article-header (done)

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h3 class = 'leading-4 mt-0 mr-0 mb-4 ml-0 text-xl pt-0 pr-0 pb-0 pl-3 uppercase border-solid border-rose-800 border-l-8'>RESULTS</h3>
+        </div>
+    );
+}
+
+export default Header;
